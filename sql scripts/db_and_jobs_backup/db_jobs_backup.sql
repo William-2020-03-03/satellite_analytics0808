53401c805694a6a2cea3a027a2ed8f9837a18ff0
William <william.qian108@gmail.com>
2025-08-13 15:55:54 +0800

    db backup scripts and jobs backup scripts

---
+++ sql scripts/db_and_jobs_backup/db_jobs_backup.sql
@@ -0,0 +1,13 @@
+﻿-- 备份业务数据库
+BACKUP DATABASE [lr_satellite]
+TO DISK = N'C:\Backup\lr_satellite_full.bak'
+WITH FORMAT, INIT, COMPRESSION, 
+     NAME = N'lr_satellite-Full Database Backup',
+     STATS = 10;
+
+-- 备份 msdb 数据库（包含 SQL Agent Jobs 等元数据）
+BACKUP DATABASE [msdb]
+TO DISK = N'c:\Backup\msdb_full.bak'
+WITH FORMAT, INIT, COMPRESSION, 
+     NAME = N'msdb-Full Backup',
+     STATS = 10;
